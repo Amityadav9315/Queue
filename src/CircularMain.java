ebffa3185b86e1b514051c2f07be58fd4d7faac0
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-13 11:17:17 +0530

    circular Queue

---
+++ src/CircularMain.java
@@ -0,0 +1,12 @@
+public class CircularMain {
+    public static void main(String[] args) {
+        CircularQueue circularQueue=new CircularQueue(5);
+        circularQueue.enqueue(19);
+        circularQueue.enqueue(50);
+        circularQueue.enqueue(65);
+        circularQueue.enqueue(76);
+        circularQueue.enqueue(876);
+        circularQueue.display();
+
+    }
+}
