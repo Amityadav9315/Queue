e8638c420ecf368208acd94ccc0bea3e6233fcb0
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-13 11:05:32 +0530

    Queue

--- src/SimpleQueue.java
+++ src/SimpleQueue.java
@@ -5,7 +5,24 @@ public class SimpleQueue {
     private int rear;
     private  int capacity;
 
-    public SimpleQueue(){}
-    if(Quei)
+    public SimpleQueue(int capacity){
+        this .capacity=capacity;
+        this.queue=new int[capacity];
+        this.front=1;
+        this.rear=-1;
+
+    }
+    //insertin data at the rear Enque
+     public void enqueue(int data){
+        if(rear==capacity-1){
+            System.out.println("Queue is full");
+            return;
+        }
+        rear++;
+        queue[rear]=data;
+         System.out.println(data+"this is inserted into the queue");
+
+     }
+
 
 }
