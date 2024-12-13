fd07188d1769c720b89b0eb57f3f70a94877a474
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-13 11:13:40 +0530

    Q,ueue

--- src/CircularQueue.java
+++ src/CircularQueue.java
@@ -20,6 +20,40 @@ public class CircularQueue {
             System.out.println("Queue is full");
             return;
         }
+        //capacity=10
+        //rear (9+1)%10===>0
+        rear=(rear+1)%capacity;
+        queue[rear]=data;
+        size++;
 
+    }
+    //delete from front
+    public int dequeue(){
+        if(size==0){
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        //actual delete
+        int data=queue[front];
+        front=(front+1)%capacity;
+        size--;
+        return data;
+    }
+    //return no of element
+    public int sixe(){
+        return this.size;
+    }
+    public  void display(){
+        if(this.size==0){
+            System.out.println("Queue is empty");
+            return;
+
+
+        }
+        for(int i=front; i<=rear; i++){
+            System.out.print(queue[i]+ "\t");
+        }
+        System.out.println();
     }
 }
