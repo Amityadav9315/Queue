d358d5b53428176e8019378ee52aebe07594bbed
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-16 00:59:43 +0530

    queue uoeration

--- src/DeQueue.java
+++ src/DeQueue.java
@@ -2,51 +2,87 @@ import java.util.Deque;
 
 public class DeQueue {
 
-    private  int[] arr;
-    private int  capacity;  //max no of element
+    private int[] arr;
+    private int capacity;  //max no of element
     private int front;
     private int rear;
-      private int size; //current no of element
+    private int size; //current no of element
 
-    public DeQueue(int capacity){
-        this.capacity=capacity;
-        arr=new int[this.capacity];
-        front=-1;
-        rear=-1;
-        size=0;
+    public DeQueue(int capacity) {
+        this.capacity = capacity;
+        arr = new int[this.capacity];
+        front = -1;
+        rear = -1;
+        size = 0;
     }
-    public boolean isFull(){
-        return size==capacity;
 
-
+    public boolean isFull() {
+        return size == capacity;
 
 
     }
 
     //check if queue is empty or not
-    public boolean isEmpty(){
-        return size==0;
+    public boolean isEmpty() {
+        return size == 0;
     }
-    public void insertFront(int data){
+
+    public void insertFront(int data) {
         //check : full
-        if(isFull()){
+        if (isFull()) {
             System.out.println("Queue is full");
             return;
         }
-        if(isEmpty()){
-            front=0;
-            rear=0;
-        }
-
-        else if(front==0){
-            front=capacity-1;
-        }
-        else{
+        if (isEmpty()) {
+            front = 0;
+            rear = 0;
+        } else if (front == 0) {
+            front = capacity - 1;
+        } else {
             front--;
         }
-        arr[front]=data;
+        arr[front] = data;
         size++;
     }
+
+    //insertion at rear end
+    public void insertRear(int data) {
+        if (isFull()) {
+            System.out.println("Queue is full");
+            return;
+        }
+        if (isEmpty()) {
+            front = -1;
+            rear = -1;
+
+        } else if (rear == capacity - 1) {
+            rear = 0;
+
+
+        } else {
+            rear++;
+        }
+        arr[rear] = data;
+        size++;
+    }
+
+    //deletion at front end
+    public int deleteFront() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        int data = arr[front];
+        if (front == rear) {
+            rear = -1;
+            front = -1;
+        } else if (front == capacity - 1) {
+            front = 0;
+        } else {
+            front++;
+        }
+    }
+
 
 
 
