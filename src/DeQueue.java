91f3e9c59d0518310ae613c0f0ef682e617f2cd3
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-16 00:38:07 +0530

    queue

--- src/DeQueue.java
+++ src/DeQueue.java
@@ -1,11 +1,23 @@
+import java.util.Deque;
+
 public class DeQueue {
 
     private  int[] arr;
-    private int  capacity;
+    private int  capacity;  //max no of element
     private int front;
     private int rear;
-      private int size;
-       public  void deQueue(){
+      private int size; //current no of element
 
-       }
+    public DeQueue(int capacity){
+        this.capacity=capacity;
+        arr=new int[this.capacity];
+        front=-1;
+        rear=-1;
+        size=0;
+    }
+
+
+
+
+
 }
