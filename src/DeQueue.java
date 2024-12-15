260802ffc374941988fda66399934ea4cc7df2ce
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-16 00:34:03 +0530

    queue

---
+++ src/DeQueue.java
@@ -0,0 +1,11 @@
+public class DeQueue {
+
+    private  int[] arr;
+    private int  capacity;
+    private int front;
+    private int rear;
+      private int size;
+       public  void deQueue(){
+
+       }
+}
