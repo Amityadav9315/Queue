7e2808e7b23446f779b5927975108ad1d436bc1d
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-01-09 22:12:06 +0530

    kglvkjjkll

--- src/SimpleQueue.java
+++ src/SimpleQueue.java
@@ -49,7 +49,6 @@ public class SimpleQueue {
         if(front>rear){
             System.out.println("Queue is empty....");
 
-            System.out.println( );
         }
         for(int i=front; i<=rear; i++) {
             System.out.print(queue[i] +"\t");
